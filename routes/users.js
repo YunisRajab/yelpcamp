63d93d46b513bdd9bacacf07b0bd2dea558bbc80
Yunis Rajab <yunis.rajab@gmail.com>
2021-03-31 20:01:16 -0400

    ready to try deploying

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const catchAsync = require("../utils/catchAsync");
+const User = require("../models/user");
+const users = require("../controllers/users");
+
+router
+  .route("/register")
+  .get((req, res) => {
+    res.render("users/register");
+  })
+  .post(catchAsync(users.renderRegister));
+
+router
+  .route("/login")
+  .get(users.renderLogin)
+  .post(
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),
+    users.login
+  );
+
+router.get("/logout", users.logout);
+
+module.exports = router;
